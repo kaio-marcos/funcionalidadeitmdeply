64d9a0a850487f583add6d402e89010ed09b6539
kaio-moreira <kaiomarcos2015@outlook.com>
2019-10-11 11:55:55 -0300

    adicionando a porra de uma funcionalidade

--- novo.py
+++ novo.py
@@ -2,7 +2,8 @@ import re
 def removertags(arquivo):
     count = 0
     #tags_removidas = open('removidos.txt', 'w')
-    planta = open('planta_certa.txt', 'a', encoding='UTF-8') 
+    #planta = open('planta_certa.txt', 'a', encoding='UTF-8')
+    planta = open('planta_ULTIMA.txt', 'w', encoding='UTF-8') 
 
     arqui = open(arquivo)
     #for i in arqui:
@@ -11,12 +12,16 @@ def removertags(arquivo):
     #    z = re.sub("\n", "", str(txt))
     #    planta.write(z)
     
+    #for i in arqui:
+    #    txt = i
+    #    z = re.sub("<path.+?>", "", str(txt))
+    #    planta.write(z)
+
     for i in arqui:
-        txt = i
-        z = re.sub("<path.+?>", "", str(txt))
+        z = re.sub("<", "\n<", str(i))
         planta.write(z)
 
     arqui.close()
     #planta.close()
 
-removertags('planta_nova.txt')
+removertags('planta_certa.svg')
